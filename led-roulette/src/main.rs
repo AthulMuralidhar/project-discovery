ae57f77f910200e26b01b6b714adefc2490ac7f5
Athul Muralidhar <athul.muralidhar@print.com>
2023-03-20 22:49:11 +0100

    pretty wild so far! i like it :)

--- led-roulette/src/main.rs
+++ led-roulette/src/main.rs
@@ -4,10 +4,15 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-// use microbit as _;
 
+// NOTES:
+// 
 // cargo run with --target thumbv7em-none-eabihf
-
+// to check if the build is an ARM binary:  cargo readobj --target thumbv7em-none-eabihf --bin led-roulette -- --file-headers
+// 
+// debugging with gdb: 
+// - it runs at localhost:1337 so, type: target remote :1337 in the gdb repl
+// - should use cmd: gdb-multiarch with ubuntu - gdb with fedora. ex:  gdb-multiarch target/thumbv7em-none-eabihf/debug/led-roulette
 
 #[entry]
 fn main() -> ! {
@@ -18,6 +23,9 @@ fn main() -> ! {
     let _y;
     _y = x;
 
+
+
+// will try to halt gdb here lol
     loop {}
     
 }
