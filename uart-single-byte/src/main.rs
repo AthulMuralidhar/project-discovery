e5a0057958a2de1030f67b303f070037480a3b6d
Athul Muralidhar <athul.muralidhar@print.com>
2023-04-09 17:10:42 +0200

    wip with uart

---
+++ uart-single-byte/src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+}
+mod serial_setup;
+use serial_setup::UartePort;
+
+// What is UARTE?
+// The microcontroller has a peripheral called UART, which stands for Universal Asynchronous Receiver/Transmitter
+
+#[entry]
+fn main() -> !{
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    nb::block!(serial.write(b'x')).unwrap();
+    nb::block!(serial.flush()).unwrap();
+}
